ede779d4cfa86df2cab883fb14769682d180b957
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-10-03 22:37:36 -0500

    sec 8, unit 132-adding the build component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
+const burger = (props) => {
+    // will grab keys from object and turn them into an array
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            // constructing an array..(3) will give you three elements for ex.
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                 return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
+    return(
+        <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top'/>
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom'/>
+        </div>
+    );
+
+};
+
+export default burger;
