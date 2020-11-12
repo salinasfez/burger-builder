475b6c63f73519449f48e9d30fad9b63f40a8d01
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-11-11 19:59:07 -0600

    section 13 doneeeeeee, REDUX starts tomorrow

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,17 +3,23 @@ import classes from './Input.module.css';
 
 const input = (props) => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
+
     switch (props.elementType){
         case('input'):
-        inputElement = <input className={classes.InputElement}{...props.elementConfig} value={props.value}/>
+        inputElement = <input className={inputClasses.join(' ')}{...props.elementConfig} value={props.value} onChange={props.changed}/>
         break;
        case('textarea'):
-        inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value}/>
+        inputElement = <textarea className={inputClasses.join(' ')} {...props.elementConfig} value={props.value}onChange={props.changed}/>
         break;
         case('select'):
         inputElement = (
                            <select 
-                                className={classes.InputElement} 
+                                onChange={props.changed}
+                                className={inputClasses.join(' ')} 
                                 value={props.value}>
                                 {props.elementConfig.options.map(option => (
                                     <option key={option.value} value={option.value}>
@@ -24,7 +30,7 @@ const input = (props) => {
         );
         break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>
+            inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value}/>
 }
         return(
         <div className={classes.Input}>
