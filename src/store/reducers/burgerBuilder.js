57c6d66a9b1125e902f8e5e8c188fa3e1dcd1e32
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-11-28 13:04:13 -0600

    refactored reducers.. only some

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const initialState = {
     ingredients: null,
@@ -15,15 +16,14 @@ const INGREDIENT_PRICES = {
 const reducer = (state = initialState, action) => {
     switch (action.type){
         case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    //dynamically overrides a property(ingredient) in an object ..ingredient name is payload
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
+        const updatedIngredient = {[action.ingredientName]: state.ingredients[action.ingredientName] + 1}
+        //state.ing is old ingredi
+        const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+        const updatedState = {
+            ingredients: updatedIngredients,
+            totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+        }
+            return updateObject(state, updatedState);
         case actionTypes.REMOVE_INGREDIENT:
             return {
                 ...state,
